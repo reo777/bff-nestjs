6039dca35266e154bb1134623fcd5ad7d14e266b
reo777 <nareto1125@gmail.com>
2021-01-02 14:38:03 +0900

    add service

---
+++ src/services/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, Task } from '@prisma/client';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class TaskService {
+  constructor(private prisma: PrismaService) {}
+
+  async createTask(data: Prisma.TaskCreateInput): Promise<Task> {
+    return this.prisma.task.create({ data });
+  }
+
+  async getAllTasks(): Promise<Task[]> {
+    return this.prisma.task.findMany();
+  }
+
+  async getOneTask(id: number): Promise<Task> {
+    return this.prisma.task.findUnique({ where: { id } });
+  }
+
+  async updateTask(id: number, data: Prisma.TaskUpdateInput) {
+    return this.prisma.task.update({ where: { id }, data });
+  }
+}
